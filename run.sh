6dbb8d151f99c84eb066df87f82e89f5a6cf52c3
Bruce Hamilton <bruce@bardly.io>
2019-06-21 06:54:05 +1000

    Fixed run script.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[[ -f snowx.jar ]] || mvn package
+[[ -f snowx.jar ]] || (mvn package && mv target/snowx-1.0-SNAPSHOT.jar snowx.jar)
 echo "Running java -jar snowx.jar $*"
 java -jar snowx.jar $*
